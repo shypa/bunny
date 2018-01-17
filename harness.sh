f5c824dd32bae86cda282c57154421ce81ebdc9d
ypsah <asyph@tutanota.com>
2018-01-18 00:30:51 +0100

    Initial commit: source files, tests and documentation
    The project is licensed under the MIT License
    
    Signed-off-by: ypsah <asyph@tutanota.com>

---
+++ harness.sh
@@ -0,0 +1,190 @@
+#! /bin/bash
+#
+# Copyright (c) 2017-2018 ypsah
+# Licensed under MIT (https://github.com/ypsah/bunny/blob/master/LICENSE.txt)
+#
+
+function fail()
+{
+    local message="$1"
+    shift
+
+    printf "failure: $message\n" "$@" >&2
+    exit 1
+}
+
+function skip()
+{
+    local message="$1"
+    shift
+
+    printf "skipping: $message\n" "$@" >&2
+    exit 75 # EX_TEMPFAIL
+}
+
+function assert_error()
+{
+    local message="$1"
+    shift
+
+    printf "assertion error: $message\n" "$@" >&2
+
+    local -i i=2
+    printf "from: " >&2
+    while ((i < ${#BASH_SOURCE[@]} - 1)); do
+        [ $i -gt 2 ] && printf "%6s" ""
+        printf "l.%i " "${BASH_LINENO[i-1]}" >&2
+        printf "%s: " "${BASH_SOURCE[i]}" >&2
+        printf "%s\n" "${FUNCNAME[i]}" >&2
+        i+=1
+    done
+    exit 1
+}
+
+function assert_true()
+{
+    "$@" || assert_error "'%s' failed with the return code %i" "$*" "$?"
+}
+
+function assert_false()
+{
+    "$@" && assert_error "'%s' did not fail" "$*"
+}
+
+function assert_equal()
+{
+    [ "$1" == "$2" ] || assert_error "'%s' != '%s'" "$1" "$2"
+}
+
+function assert_not_equal()
+{
+    [ "$1" != "$2" ] || assert_error "'%s' == '%s'" "$1" "$2"
+}
+
+function assert_empty()
+{
+    [ -z "$1" ] || assert_error "'%s' is not empty" "$1"
+}
+
+function assert_not_empty()
+{ 
+    [ -n "$1" ] || assert_error "'%s' is empty" "$1"
+}
+
+function assert_in()
+{
+    local target="$1"
+    shift
+
+    for element in "$@"; do
+        [ "$element" == "$target" ] && return
+    done
+
+    local array
+    printf -v array "'%s' " "$@"
+    assert_error "'%s' is not in (%s)" "$target" "${array% }"
+}
+
+function assert_not_in()
+{
+    local target="$1"
+    shift
+
+    for element in "$@"; do
+        if [ "$element" == "$target" ]; then
+            local array
+            printf -v array "'%s' " "$@"
+            assert_error "'%s' is in (%s)" "$target" "${array% }"
+        fi
+    done
+}
+
+function assert_greater()
+{
+    [ "$1" -gt "$2" ] || assert_error "%s <= %s" "$1" "$2"
+}
+
+function assert_greater_equal()
+{
+    [ "$1" -ge "$2" ] || assert_error "%s < %s" "$1" "$2"
+}
+
+function assert_less()
+{
+    [ "$1" -lt "$2" ] || assert_error "%s >= %s" "$1" "$2"
+}
+
+function assert_less_equal()
+{
+    [ "$1" -le "$2" ] || assert_error "%s > %s" "$1" "$2"
+}
+
+function assert_exists()
+{
+    [ -e "$1" ] || assert_error "'%s' does not exist" "$1"
+}
+
+function assert_missing()
+{
+    [ ! -e "$1" ] || assert_error "'%s' exists" "$1"
+}
+
+function assert_file()
+{
+    [ -f "$1" ] || assert_error "'%s' is not a file" "$1"
+}
+
+function assert_not_file()
+{
+    [ ! -f "$1" ] || assert_error "'%s' is a file" "$1"
+}
+
+function assert_directory()
+{
+    [ -d "$1" ] || assert_error "'%s' is not a directory" "$1"
+}
+
+function assert_not_directory()
+{
+    [ ! -d "$1" ] || assert_error "'%s' is a directory" "$1"
+}
+
+function assert_empty_directory()
+{
+    assert_directory "$1"
+
+    (
+        shopt -s dotglob nullglob
+        for _ in "$1"/*; do
+            exit 1
+        done
+    ) || assert_error "'%s' is not an empty directory" "$1"
+}
+
+function assert_not_empty_directory()
+{
+    assert_directory "$1"
+
+    (
+        shopt -s dotglob nullglob
+        for _ in "$1"/*; do
+            exit 0
+        done
+        exit 1
+    ) || assert_error "'%s' is an empty directory" "$1"
+}
+
+function stack_trap()
+{
+    local arg="$1"
+    local sigspec="$2"
+
+    local oldtrap="$(trap -p "$sigspec")"
+    oldtrap="${oldtrap:+"; ${oldtrap#trap -- \'}"}"
+
+    local newtrap="$(trap -- "$arg" "$sigspec"
+                     trap -p "$sigspec"
+                     trap -- '' "$sigspec")"
+
+    eval "${newtrap%\' $sigspec}${oldtrap:-"' $sigspec"}"
+}
---
+++ test_harness.sh
@@ -0,0 +1,220 @@
+#! /bin/bash
+#
+# Copyright (c) 2017-2018 ypsah
+# Licensed under MIT (https://github.com/ypsah/bunny/blob/master/LICENSE.txt)
+#
+
+test_fail()
+{
+    ( fail '%s %s %s %s' "This" "is" "a" "test" )
+    [ $? -eq 1 ]
+}
+
+test_skip()
+{
+    ( skip '%s %s %s %s' "This" "is" "a" "test" )
+    [ $? -eq 75 ]
+}
+
+test_true()
+{
+    assert_true true
+    ( assert_true false )
+    [ $? -eq 1 ]
+}
+
+test_false()
+{
+    assert_false false
+    ( assert_false true )
+    [ $? -eq 1 ]
+}
+
+test_equal()
+{
+    assert_equal abc abc
+    ( assert_equal abc bca )
+    [ $? -eq 1 ]
+}
+
+test_not_equal()
+{
+    assert_not_equal abc bca
+    ( assert_not_equal abc abc )
+    [ $? -eq 1 ]
+}
+
+test_empty()
+{
+    assert_empty
+    ( assert_empty " " )
+    [ $? -eq 1 ]
+}
+
+test_not_empty()
+{
+    assert_not_empty " "
+    ( assert_not_empty )
+    [ $? -eq 1 ]
+}
+
+test_in()
+{
+    local array=("abc" "def" "ghi")
+    assert_in "def" "${array[@]}"
+    ( assert_in "a" "${array[@]}" )
+    [ $? -eq 1 ]
+}
+
+test_not_in()
+{
+    local array=("abc" "def" "ghi")
+    assert_not_in "a" "${array[@]}"
+    ( assert_not_in "def" "${array[@]}" )
+    [ $? -eq 1 ]
+}
+
+test_greater()
+{
+    assert_greater "1" "0"
+    ( assert_greater "0" "0" )
+    [ $? -eq 1 ]
+}
+
+test_greater_equal()
+{
+    assert_greater_equal "1" "0"
+    assert_greater_equal "0" "0"
+    ( assert_greater_equal "0" "1" )
+    [ $? -eq 1 ]
+}
+
+test_less()
+{
+    assert_less "0" "1"
+    ( assert_less "0" "0" )
+    [ $? -eq 1 ]
+}
+
+test_less_equal()
+{
+    assert_less_equal "0" "1"
+    assert_less_equal "0" "0"
+    ( assert_less_equal "1" "0" )
+    [ $? -eq 1 ]
+}
+
+test_exists()
+{
+    local file="$(mktemp)"
+    stack_trap "rm -f '$file'" EXIT
+
+    assert_exists "$file"
+    rm "$file"
+    ( assert_exists "$file" )
+    [ $? -eq 1 ]
+}
+
+test_missing()
+{
+    local file="$(mktemp)"
+    stack_trap "rm -f '$file'" EXIT
+
+    ( assert_missing "$file" ) && exit 1
+    rm "$file"
+    assert_missing "$file"
+}
+
+test_file()
+{
+    local file="$(mktemp)"
+    stack_trap "rm -f '$file'" EXIT
+
+    assert_file "$file"
+
+    local directory="$(mktemp -d)"
+    stack_trap "rm -rf '$directory'" EXIT
+
+    ( assert_file "$directory" )
+    [ $? -eq 1 ]
+}
+
+test_not_file()
+{
+    local directory="$(mktemp -d)"
+    stack_trap "rm -rf '$directory'" EXIT
+
+    assert_not_file "$directory"
+
+    local file="$(mktemp)"
+    stack_trap "rm -f '$file'" EXIT
+
+    ( assert_not_file "$file" )
+    [ $? -eq 1 ]
+}
+
+test_directory()
+{
+    local directory="$(mktemp -d)"
+    stack_trap "rm -rf '$directory'" EXIT
+
+    assert_directory "$directory"
+
+    local file="$(mktemp)"
+    stack_trap "rm -f '$file'" EXIT
+
+    ( assert_directory "$file" )
+    [ $? -eq 1 ]
+}
+
+test_not_directory()
+{
+    local file="$(mktemp)"
+    stack_trap "rm -f '$file'" EXIT
+
+    assert_not_directory "$file"
+
+    local directory="$(mktemp -d)"
+    stack_trap "rm -rf '$directory'" EXIT
+
+    ( assert_not_directory "$directory" )
+    [ $? -eq 1 ]
+}
+
+test_empty_directory()
+{
+    local directory="$(mktemp -d)"
+    stack_trap "rm -rf '$directory'" EXIT
+
+    assert_empty_directory "$directory"
+
+    local file="$(mktemp)"
+    stack_trap "rm -f '$file'" EXIT
+
+    ( assert_empty_directory "$file" )
+    [ $? -eq 1 ] || exit 1
+
+    touch "$directory/${file##*/}"
+
+    ( assert_empty_directory "$directory" )
+    [ $? -eq 1 ] || exit 1
+}
+
+test_not_empty_directory()
+{
+    local directory="$(mktemp -d)"
+    stack_trap "rm -rf '$directory'" EXIT
+
+    ( assert_not_empty_directory "$directory" )
+    [ $? -eq 1 ] || exit 1
+
+    local file="$(mktemp)"
+    stack_trap "rm -f '$file'" EXIT
+
+    ( assert_not_empty_directory "$file" )
+    [ $? -eq 1 ] || exit 1
+
+    touch "$directory/${file##*/}"
+
+    assert_not_empty_directory "$directory"
+}
